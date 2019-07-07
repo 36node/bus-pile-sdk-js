417c18d6e99d3da8cc4b9ce295e8ba553ac6e948
Li Dong <lidongbest5@gmail.com>
2019-07-07 17:56:39 +0800

    add test

--- src/index.test.js
+++ src/index.test.js
@@ -2,37 +2,14 @@ import SDK from "./index";
 
 const sdk = new SDK({ base: "http://localhost:3000" });
 
-describe("## SDK vehicle", () => {
-  it("should list pets", async () => {
-    const result = await sdk.pet.listPets();
+describe("## SDK Pile", () => {
+  it("should list stations", async () => {
+    const result = await sdk.station.listStations();
+    expect(result.body.length).toBe(10);
+  });
+
+  it("should list piles", async () => {
+    const result = await sdk.pile.listPiles();
     expect(result.body.length).toBe(100);
-  });
-
-  let pet;
-
-  it("should create pet", async () => {
-    const newPet = {
-      name: "jam",
-      tag: "DOG",
-      owner: "lily",
-    };
-
-    const result = await sdk.pet.createPet({ body: newPet });
-    pet = result.body;
-    expect(pet).toMatchObject(newPet);
-  });
-
-  it("should get pet", async () => {
-    const result = await sdk.pet.showPetById({
-      petId: pet.id,
-    });
-    expect(result.body.id).toBe(pet.id);
-  });
-
-  it("should delete pet", async () => {
-    const result = await sdk.pet.deletePet({
-      petId: pet.id,
-    });
-    expect(result.body).toEqual({});
   });
 });
